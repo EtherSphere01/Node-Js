94a0e6dcc30d1710d6792786c4a4a6d9bb6dece0
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-03-13 17:35:12 +0600

    'cf'

--- Practice/asyncAwait.js
+++ Practice/asyncAwait.js
@@ -1,20 +1,61 @@
 console.log("start");
 
 async function sum() {
-  let s = 0;
-    setTimeout(() => {
-         for (i = 0; i < 2000; i++) {
-           sum += i;
-         }
-         return sum;
-    }, 2000);
+  setTimeout(() => {
+    let s = 0;
+    for (let i = 0; i < 2000; i++) {
+      s += i;
+    }
+    console.log(s);
+  }, 2000);
+}
+
+async function a() {
+  setTimeout(() => {
+    console.log("inside a");
+  }, 2000);
+}
+async function b() {
+  setTimeout(() => {
+    console.log("inside b");
+  }, 2000);
+}
+async function c() {
+  setTimeout(() => {
+    console.log("inside c");
+  }, 2000);
 }
 
 async function check() {
   console.log("inside async function");
-  let sum = await sum();
-  console.log(sum);
+  await sum();
+  await a();
+  await b();
+  await c();
   console.log("end");
 }
 check();
 console.log("outside async function");
+
+// ////////////////////////////
+
+function first(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let s = 0;
+      for (let i = 0; i <= n; i++) {
+        s += i;
+      }
+      console.log(s);
+      resolve(s);
+    }, 5000);
+  });
+}
+
+async function call() {
+  console.log("inside call");
+  await first(100);
+  console.log("after first");
+  await first(200);
+}
+call();
