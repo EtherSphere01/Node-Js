a8e4ae10d7fd3eeb1e9f3b157a06d97f99d7f773
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-03-13 19:25:07 +0600

    'API'

---
+++ CLI application/randomJokegenerator.js
@@ -0,0 +1,27 @@
+import https from "https";
+import chalk from "chalk";
+
+function getNewjoke() {
+  const getJoke = () => {
+    const url = "https://official-joke-api.appspot.com/random_joke";
+    https.get(url, (res) => {
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        console.log(chalk.blue("Joke of the day"));
+        console.log(chalk.green(JSON.parse(data).setup));
+        console.log(chalk.red(JSON.parse(data).punchline));
+        console.log(chalk.blue("Thank you for using the joke generator!"));
+      });
+    });
+  };
+  getJoke();
+}
+
+async function displayJoke() {
+  await getNewjoke();
+}
+
+displayJoke();
