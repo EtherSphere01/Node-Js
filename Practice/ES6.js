ec7cc0692d4623ebec291fe6451ee93addf18b65
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-03-16 20:05:22 +0600

    practice

--- Practice/ES6.js
+++ Practice/ES6.js
@@ -2,3 +2,11 @@ let a = 10;
 var b = 20;
 const c = 30;
 
+const fun = () => {
+    let a = 50;
+    var b = 60;
+    const c = 70;
+    console.log(a, b, c);
+};
+fun();
+console.log(a, b, c); // 10 20 30
